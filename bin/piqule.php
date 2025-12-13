66ae40219e2ab2a203f8263e7224a639f3a050fa
Kanstantsin Mesnik <haspadar@gmail.com>
2025-12-13 13:39:12 +0300

    feat(cli): add piqule command entrypoint

---
+++ bin/piqule.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Haspadar\Piqule\Command\Init;
+use Haspadar\Piqule\Command\Update;
+use Haspadar\Piqule\FileSystem\DiskFileSystem;
+use Haspadar\Piqule\Output\Console;
+use Haspadar\Piqule\Output\Line\Error;
+use Haspadar\Piqule\Structure\Root;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$output = new Console();
+if (!isset($argv[1])) {
+    $output->write(new Error('Usage: piqule <init|update>'));
+    exit(1);
+}
+
+$argument = $argv[1];
+$root = new Root(getenv('COMPOSER_CWD') ?: getcwd());
+match ($argument) {
+    'init' => (new Init($root, new DiskFileSystem(), $output))->run(),
+    'update' => (new Update($root, new DiskFileSystem(), $output))->run(),
+    default => $output->write(new Error("Unknown command: $argument\n"))
+};
