d4105be5aa7ef4d5f91e16c60f4f7e81495b21ac
Kanstantsin Mesnik <haspadar@gmail.com>
2025-12-25 12:44:54 +0300

    test(command): add unit tests for synchronization

---
+++ tests/Fake/File/FakeFile.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Haspadar\Piqule\Tests\Fake\File;
+
+use Haspadar\Piqule\File\File;
+
+final readonly class FakeFile implements File
+{
+    public function __construct(
+        private string $contents,
+    ) {}
+
+    public function contents(): string
+    {
+        return $this->contents;
+    }
+}
---
+++ tests/Fake/Source/FakeSources.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Haspadar\Piqule\Tests\Fake\Source;
+
+use Haspadar\Piqule\Source\Source;
+use Haspadar\Piqule\Source\Sources;
+
+final class FakeSources implements Sources
+{
+    /** @var list<Source> */
+    private array $files;
+
+    /**
+     * @param list<Source> $files
+     */
+    public function __construct(array $files)
+    {
+        $this->files = $files;
+    }
+
+    public function files(): iterable
+    {
+        return $this->files;
+    }
+}
