1bb9c3c18533c062de292889f6f214b4f0bebce1
Kanstantsin Mesnik <haspadar@gmail.com>
2026-01-11 02:58:36 +0300

    Remove legacy CLI entrypoints to avoid split command logic

--- src/Cli.php
+++ src/Cli.php
@@ -6,6 +6,9 @@ namespace Haspadar\Piqule;
 
 final readonly class Cli
 {
+    /**
+     * @param array<int, string> $argv
+     */
     public function __construct(
         private array $argv,
     ) {}
