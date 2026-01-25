65fc74c0b94626ab3955bc09b6d189695d7bc797
Kanstantsin Mesnik <haspadar@gmail.com>
2026-01-25 12:20:12 +0300

    Introduce ExecutableFile with tests to make executable behavior explicit

---
+++ src/File/ExecutableFile.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Haspadar\Piqule\File;
+
+use Haspadar\Piqule\Storage\Storage;
+use Override;
+
+final readonly class ExecutableFile implements File
+{
+    public function __construct(
+        private File $origin,
+    ) {}
+
+    #[Override]
+    public function name(): string
+    {
+        return $this->origin->name();
+    }
+
+    #[Override]
+    public function contents(): string
+    {
+        return $this->origin->contents();
+    }
+
+    #[Override]
+    public function writeTo(Storage $storage): void
+    {
+        $storage->writeExecutable(
+            $this->name(),
+            $this->contents(),
+        );
+    }
+}
