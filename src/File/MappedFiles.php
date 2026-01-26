0ac7fe0027b01cc4aa564d39bd6b949c478eb73c
Kanstantsin Mesnik <haspadar@gmail.com>
2026-01-26 11:56:20 +0300

    Add MappedFiles to apply per-file transformations without introducing orchestration logic

---
+++ src/File/MappedFiles.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Haspadar\Piqule\File;
+
+use Closure;
+use Override;
+
+final readonly class MappedFiles implements Files
+{
+    /**
+     * @param Closure(File): File $map
+     */
+    public function __construct(
+        private Files $origin,
+        private Closure $map,
+    ) {}
+
+    #[Override]
+    public function all(): iterable
+    {
+        foreach ($this->origin->all() as $file) {
+            yield ($this->map)($file);
+        }
+    }
+}
