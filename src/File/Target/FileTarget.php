44d1b1ac4f66303f2769312714f2d61b0a3749f6
Kanstantsin Mesnik <haspadar@gmail.com>
2026-01-27 11:22:54 +0300

    Add FileSkipped event to represent skipped file handling

--- src/File/Target/FileTarget.php
+++ src/File/Target/FileTarget.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Haspadar\Piqule\File\Target;
 
 use Haspadar\Piqule\File\Event\FileCreated;
+use Haspadar\Piqule\File\Event\FileSkipped;
 use Haspadar\Piqule\File\Event\FileUpdated;
 
 interface FileTarget
@@ -12,4 +13,6 @@ interface FileTarget
     public function created(FileCreated $event): void;
 
     public function updated(FileUpdated $event): void;
+
+    public function skipped(FileSkipped $event): void;
 }
