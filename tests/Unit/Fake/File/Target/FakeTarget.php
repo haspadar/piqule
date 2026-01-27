dcf465bef5ec4921722bc73bd783745860abd0e1
Kanstantsin Mesnik <haspadar@gmail.com>
2026-01-27 10:34:35 +0300

    Add FakeTarget to support file event unit tests

---
+++ tests/Unit/Fake/File/Target/FakeTarget.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Haspadar\Piqule\Tests\Unit\Fake\File\Target;
+
+use Haspadar\Piqule\File\Event\FileCreated;
+use Haspadar\Piqule\File\Event\FileSkipped;
+use Haspadar\Piqule\File\Event\FileUpdated;
+use Haspadar\Piqule\File\Target\FileTarget;
+
+final class FakeTarget implements FileTarget
+{
+    /** @var list<object> */
+    private array $events = [];
+
+    public function created(FileCreated $event): void
+    {
+        $this->events[] = $event;
+    }
+
+    public function updated(FileUpdated $event): void
+    {
+        $this->events[] = $event;
+    }
+
+    public function skipped(FileSkipped $event): void
+    {
+        $this->events[] = $event;
+    }
+
+    /**
+     * @return list<object>
+     */
+    public function events(): array
+    {
+        return $this->events;
+    }
+}
