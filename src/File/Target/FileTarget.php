6c16297e93004ae8d39f28b14407439d282683af
Kanstantsin Mesnik <haspadar@gmail.com>
2026-01-27 10:36:58 +0300

    Limit FileTarget to existing file events

--- src/File/Target/FileTarget.php
+++ src/File/Target/FileTarget.php
@@ -5,14 +5,9 @@ declare(strict_types=1);
 namespace Haspadar\Piqule\File\Target;
 
 use Haspadar\Piqule\File\Event\FileCreated;
-use Haspadar\Piqule\File\Event\FileSkipped;
-use Haspadar\Piqule\File\Event\FileUpdated;
 
 interface FileTarget
 {
     public function created(FileCreated $event): void;
 
-    public function updated(FileUpdated $event): void;
-
-    public function skipped(FileSkipped $event): void;
 }
