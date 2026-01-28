56a2ad88cc287d0cdac025697d525936a2ad3a81
Kanstantsin Mesnik <haspadar@gmail.com>
2026-01-28 13:09:06 +0300

    Add CompositeFiles to combine multiple file sources

---
+++ src/File/CompositeFiles.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Haspadar\Piqule\File;
+
+use Override;
+
+final readonly class CompositeFiles implements Files
+{
+    /**
+     * @param list<Files> $sources
+     */
+    public function __construct(
+        private array $sources,
+    ) {}
+
+    #[Override]
+    public function all(): iterable
+    {
+        foreach ($this->sources as $files) {
+            yield from $files->all();
+        }
+    }
+}
