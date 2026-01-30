51ab726dfeb23f28519cb203dd338ded7c9840e8
Kanstantsin Mesnik <haspadar@gmail.com>
2026-01-30 20:42:49 +0300

    Add tests for ValidatedDirectoryPath and align expectations with normalization

--- tests/Unit/File/DirectoryPathTest.php
+++ tests/Unit/File/DirectoryPathTest.php
@@ -5,55 +5,18 @@ declare(strict_types=1);
 namespace Haspadar\Piqule\Tests\Unit\File;
 
 use Haspadar\Piqule\File\DirectoryPath;
-use Haspadar\Piqule\PiquleException;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 final class DirectoryPathTest extends TestCase
 {
     #[Test]
-    public function throwsWhenEmpty(): void
-    {
-        $this->expectException(PiquleException::class);
-
-        (new DirectoryPath(''))->value();
-    }
-
-    #[Test]
-    public function throwsWhenRelative(): void
-    {
-        $this->expectException(PiquleException::class);
-
-        (new DirectoryPath('relative/path'))->value();
-    }
-
-    #[Test]
-    public function returnsPosixAbsolute(): void
+    public function returnsRawValue(): void
     {
         self::assertSame(
-            '/var/www',
-            (new DirectoryPath('/var/www/'))->value(),
-            'Accepts POSIX absolute directory paths',
-        );
-    }
-
-    #[Test]
-    public function returnsWindowsDriveAbsolute(): void
-    {
-        self::assertSame(
-            'C:\Windows',
-            (new DirectoryPath('C:\Windows\\'))->value(),
-            'Accepts Windows drive absolute directory paths',
-        );
-    }
-
-    #[Test]
-    public function returnsWindowsUncAbsolute(): void
-    {
-        self::assertSame(
-            '\Windows\System32',
-            (new DirectoryPath('\Windows\System32\\'))->value(),
-            'Accepts Windows UNC absolute directory paths',
+            'any/path/value',
+            (new DirectoryPath('any/path/value'))->value(),
+            'Returns raw directory path value as provided',
         );
     }
 }
