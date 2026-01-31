e75a15e09e50f0c3db6804732a0fec9c1cc800ca
Kanstantsin Mesnik <haspadar@gmail.com>
2026-01-31 10:56:07 +0300

    Add RelativeFilePath to explicitly represent file paths relative to the current context

---
+++ src/Path/File/RelativeFilePath.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Haspadar\Piqule\Path\File;
+
+use Haspadar\Piqule\File\FileName;
+use Override;
+
+final readonly class RelativeFilePath implements FilePath
+{
+    public function __construct(private FileName $name) {}
+
+    #[Override]
+    public function value(): string
+    {
+        return $this->name->value();
+    }
+}
