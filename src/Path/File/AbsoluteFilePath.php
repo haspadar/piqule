93e5064bb901dfaecd7dd4d9ce3497039c6138d3
Kanstantsin Mesnik <haspadar@gmail.com>
2026-01-31 11:14:07 +0300

    Depend on DirectoryPath abstraction in AbsoluteFilePath to allow composition with normalized and validated directory paths

--- src/Path/File/AbsoluteFilePath.php
+++ src/Path/File/AbsoluteFilePath.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace Haspadar\Piqule\Path\File;
 
 use Haspadar\Piqule\File\FileName;
-use Haspadar\Piqule\Path\Directory\AbsoluteDirectoryPath;
+use Haspadar\Piqule\Path\Directory\DirectoryPath;
 use Override;
 
 final readonly class AbsoluteFilePath implements FilePath
 {
     public function __construct(
-        private AbsoluteDirectoryPath $directory,
-        private FileName              $name,
+        private DirectoryPath $directory,
+        private FileName $name,
     ) {}
 
     #[Override]
