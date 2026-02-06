7f31fe4abf5e48967b1077a89133ae85dac504a2
Kanstantsin Mesnik <haspadar@gmail.com>
2026-02-07 00:22:04 +0300

    Add scalar replacement for single textual values

---
+++ src/Replacement/ScalarReplacement.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Haspadar\Piqule\Replacement;
+
+use Override;
+
+final readonly class ScalarReplacement implements Replacement
+{
+    public function __construct(
+        private string $value,
+    ) {}
+
+    #[Override]
+    public function value(): string
+    {
+        return $this->value;
+    }
+}
