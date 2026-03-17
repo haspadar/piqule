3f81407505cd6e6750086bedd2a0198393bff452
Kanstantsin Mesnik <haspadar@gmail.com>
2026-03-17 18:10:46 +0300

    Override PR size limit to 2000 for this repo's refactoring PRs

--- .piqule.php
+++ .piqule.php
@@ -10,7 +10,7 @@ return new AppendConfig(
     new OverrideConfig(new DefaultConfig(), [
         'phpmetrics.coupling.max_efferent' => [25],
         'ci.php.matrix' => ['8.3', '8.4', '8.5'],
-        'ci.pr.max_lines_changed' => 400,
+        'ci.pr.max_lines_changed' => 2000,
         'ci.piqule_bin' => 'bin/piqule',
     ]),
     [
