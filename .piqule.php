592be05bab43c981b21066df4f7b065110a987ff
Kanstantsin Mesnik <haspadar@gmail.com>
2026-03-17 14:32:40 +0300

    Replace per-tool overrides with dirs.exclude to eliminate duplication

--- .piqule.php
+++ .piqule.php
@@ -2,36 +2,18 @@
 
 declare(strict_types=1);
 
+use Haspadar\Piqule\Config\AppendConfig;
 use Haspadar\Piqule\Config\DefaultConfig;
 use Haspadar\Piqule\Config\OverrideConfig;
 
-return new OverrideConfig(new DefaultConfig(), [
-    'markdownlint.ignores' => [
-        '**/vendor/**',
-        '**/node_modules/**',
-        '**/coverage/**',
-        '**/.git/**',
-        '**/templates/**',
+return new AppendConfig(
+    new OverrideConfig(new DefaultConfig(), [
+        'phpmetrics.coupling.max_efferent' => [25],
+        'ci.php.matrix' => ['8.3', '8.4', '8.5'],
+        'ci.pr.max_lines_changed' => 400,
+        'ci.piqule_bin' => 'bin/piqule',
+    ]),
+    [
+        'dirs.exclude' => ['templates'],
     ],
-
-    'yamllint.ignore' => [
-        'vendor/**',
-        'node_modules/**',
-        'build/**',
-        'var/**',
-        'templates/**',
-        '.piqule/**/html/**',
-        '.piqule/**/coverage-report/**',
-    ],
-    'yamllint.line_length.max' => [120],
-
-    'phpcs.excludes' => ['vendor/*', 'tests/*', 'templates/*'],
-
-    'shellcheck.ignore_dirs' => ['vendor', '.git', 'templates'],
-
-    'phpmetrics.coupling.max_efferent' => [25],
-
-    'ci.php.matrix' => ['8.3', '8.4', '8.5'],
-    'ci.pr.max_lines_changed' => 400,
-    'ci.piqule_bin' => 'bin/piqule',
-]);
+);
