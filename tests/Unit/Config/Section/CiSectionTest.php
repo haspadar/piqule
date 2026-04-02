1b8adaff3b04db4dc4b697cd9b3031aeae96431b
Kanstantsin Mesnik <haspadar@gmail.com>
2026-04-02 12:51:00 +0300

    Merge ci.php.matrix, php.version and ci.php.test_version into single php.versions key

--- tests/Unit/Config/Section/CiSectionTest.php
+++ tests/Unit/Config/Section/CiSectionTest.php
@@ -11,26 +11,12 @@ use PHPUnit\Framework\TestCase;
 final class CiSectionTest extends TestCase
 {
     #[Test]
-    public function propagatesMatrixVersionsIndependentlyFromTestVersion(): void
+    public function propagatesPhpVersions(): void
     {
-        $section = new CiSection(['8.3', '8.4', '8.5'], ['8.3']);
-
         self::assertSame(
             ['8.3', '8.4', '8.5'],
-            $section->toArray()['ci.php.matrix'],
-            'ci.php.matrix must reflect the given matrix versions',
-        );
-    }
-
-    #[Test]
-    public function propagatesTestVersionIndependentlyFromMatrix(): void
-    {
-        $section = new CiSection(['8.3', '8.4', '8.5'], ['8.3']);
-
-        self::assertSame(
-            ['8.3'],
-            $section->toArray()['ci.php.test_version'],
-            'ci.php.test_version must reflect the given test version independently',
+            (new CiSection(['8.3', '8.4', '8.5']))->toArray()['php.versions'],
+            'php.versions must reflect the given versions list',
         );
     }
 
@@ -39,7 +25,7 @@ final class CiSectionTest extends TestCase
     {
         self::assertSame(
             250,
-            (new CiSection([], []))->toArray()['ci.pr.max_lines_changed'],
+            (new CiSection([]))->toArray()['ci.pr.max_lines_changed'],
             'ci.pr.max_lines_changed must default to 250',
         );
     }
