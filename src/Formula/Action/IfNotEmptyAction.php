8f6c910d4bbd1347edda0306d8c9f08bfdcc0615
Kanstantsin Mesnik <haspadar@gmail.com>
2026-04-04 11:04:58 +0300

    Guard action to skip pipeline when input is empty

---
+++ src/Formula/Action/IfNotEmptyAction.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Haspadar\Piqule\Formula\Action;
+
+use Haspadar\Piqule\Formula\Args\Args;
+use Haspadar\Piqule\Formula\Args\ListArgs;
+use Override;
+
+/**
+ * Passes non-empty input through, clears empty input
+ */
+final readonly class IfNotEmptyAction implements Action
+{
+    #[Override]
+    public function transformed(Args $args): Args
+    {
+        $values = $args->values();
+
+        if ($values === [] || $values === ['']) {
+            return new ListArgs([]);
+        }
+
+        return $args;
+    }
+}
