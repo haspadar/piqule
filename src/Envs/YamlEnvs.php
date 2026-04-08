bb18e28068ef5d3c949394716bd38801f3e39524
Kanstantsin Mesnik <haspadar@gmail.com>
2026-04-08 23:01:41 +0300

    Parse envs section from .piqule.yaml

---
+++ src/Envs/YamlEnvs.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Haspadar\Piqule\Envs;
+
+use Haspadar\Piqule\PiquleException;
+use Override;
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Parses the envs section from a .piqule.yaml file.
+ */
+final readonly class YamlEnvs implements Envs
+{
+    /** Initializes with the path to a .piqule.yaml file. */
+    public function __construct(private string $path) {}
+
+    #[Override]
+    public function vars(): array
+    {
+        try {
+            /** @var mixed $yaml */
+            $yaml = Yaml::parseFile($this->path);
+        } catch (ParseException $e) {
+            throw new PiquleException(
+                sprintf('Failed to parse "%s": %s', $this->path, $e->getMessage()),
+                0,
+                $e,
+            );
+        }
+
+        /** @var mixed $envs */
+        $envs = is_array($yaml)
+            ? ($yaml['envs'] ?? [])
+            : [];
+
+        if (!is_array($envs)) {
+            throw new PiquleException(
+                sprintf('Invalid "envs" section in "%s": expected a mapping', $this->path),
+            );
+        }
+
+        /** @var array<string, string> $vars */
+        $vars = [];
+
+        foreach ($envs as $name => $command) {
+            if (!is_string($name) || !is_string($command)) {
+                throw new PiquleException(
+                    sprintf('Each entry in "envs" must be string => string in "%s"', $this->path),
+                );
+            }
+
+            $vars[$name] = $command;
+        }
+
+        return $vars;
+    }
+}
