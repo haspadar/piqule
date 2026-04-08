ef53d93049ddddeaf5f6be9db27a9dbc628f1254
Kanstantsin Mesnik <haspadar@gmail.com>
2026-04-08 07:26:34 +0300

    Extract path dependencies into value object to reduce parameter count

---
+++ src/Config/ConfigPaths.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Haspadar\Piqule\Config;
+
+/**
+ * Paths used to locate configuration files
+ */
+final readonly class ConfigPaths
+{
+    public function __construct(
+        private string $composerJson = '',
+        private string $configYaml = __DIR__ . '/../../templates/always/.piqule/config.yaml',
+    ) {}
+
+    /** Returns the composer.json file path */
+    public function composerJson(): string
+    {
+        return $this->composerJson;
+    }
+
+    /** Returns the config.yaml file path */
+    public function configYaml(): string
+    {
+        return $this->configYaml;
+    }
+}
