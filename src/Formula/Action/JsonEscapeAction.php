9a358e12d10a0235be89e0c8cfb3cacea932f5a4
Kanstantsin Mesnik <haspadar@gmail.com>
2026-04-19 15:52:18 +0300

    #630: Fail json_escape loudly on malformed UTF-8 input

--- src/Formula/Action/JsonEscapeAction.php
+++ src/Formula/Action/JsonEscapeAction.php
@@ -7,6 +7,8 @@ namespace Haspadar\Piqule\Formula\Action;
 use Haspadar\Piqule\Formula\Args\Args;
 use Haspadar\Piqule\Formula\Args\ListArgs;
 use Haspadar\Piqule\Formula\Args\StringifiedArgs;
+use InvalidArgumentException;
+use JsonException;
 use Override;
 
 /**
@@ -17,15 +19,26 @@ final readonly class JsonEscapeAction implements Action
     #[Override]
     public function transformed(Args $args): Args
     {
-        return new ListArgs(
-            array_map(
-                static fn(int|float|string|bool $item): string => (string) preg_replace(
-                    '/^"|"$/',
-                    '',
-                    (string) json_encode((string) $item, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES),
+        try {
+            return new ListArgs(
+                array_map(
+                    static fn(int|float|string|bool $item): string => (string) preg_replace(
+                        '/^"|"$/',
+                        '',
+                        json_encode(
+                            (string) $item,
+                            JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES,
+                        ),
+                    ),
+                    (new StringifiedArgs($args))->values(),
                 ),
-                (new StringifiedArgs($args))->values(),
-            ),
-        );
+            );
+        } catch (JsonException $exception) {
+            throw new InvalidArgumentException(
+                sprintf('Cannot JSON-encode value: %s', $exception->getMessage()),
+                0,
+                $exception,
+            );
+        }
     }
 }
